406f60f30e1440c2acef94f5872d71cdd7e701b5
KhuramC <khuramchoudhry123@gmail.com>
2024-01-30 19:04:02 -0600

    Grammar fix.

--- Homework_1/src/Homework_1/Homework_1.java
+++ Homework_1/src/Homework_1/Homework_1.java
@@ -4,19 +4,20 @@ public class Homework_1 {
 	public String name;
 	public int GitHubID;
 	
+	//Class that holds name and ID
 	public Homework_1(String name,int GitHubID){
 		this.name = name;
 		this.GitHubID = GitHubID;
 		
 	}
-	//to string of object
+	//to string of Class
 	public String toString() {
-		String s = this.name + "has a Github ID of " + this.GitHubID +"!";
+		String s = this.name + " has a Github ID of " + this.GitHubID +"!";
 		return s;
 		
 	}
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
+		
 		Homework_1 Khuram = new Homework_1("Khuram", 112579665);
 		System.out.println(Khuram);
 		
